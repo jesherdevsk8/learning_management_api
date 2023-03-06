852842320f42cad33b0a6668ad10893557feea68
Jesher Minelli Alves <jesherdevsk8@gmail.com>
2023-03-06 02:36:20 -0300

    add validation email format and uniqueness

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -7,6 +7,7 @@ class Instructor < ApplicationRecord
   has_many :tasks
 
   validates :name, :email, :password_digest, presence: true, length: { maximum: 255 }
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must be a valid email address' }
   validates :status, inclusion: { in: %w( ativo ferias contrato_de_trabalho_suspenso aviso_previo previdencia licenca_maternidade ) }
 
   def encrypt_password
